14dcfb64503bd626e82c546292c3b3a42aa18ec1
bWyatt2026 <blake.wyatt@stu.daviess.kyschools.us>
2025-02-12 13:46:44 +0000

    Finished

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -4,20 +4,58 @@ public class ArrayAverages{
   
   int[][] ray;
 
-  public ArrayAverages(int[][] rayray){
+  public ArrayAverages(int[][] rayray)
+  {
     ray = rayray;
   }
 
-  public void rowAverages(){
-    
+  public void rowAverages()
+  {
+    int rowAverages = 0;
+    for(int row = 0; row<ray.length; row++)
+    {
+      for(int col = 0; col<ray[row].length; col++)
+      {
+        rowAverages += ray[row][col];
+      }
+      rowAverages /= ray[row].length;
+      System.out.println("The Average for row " + row + " is: " + rowAverages);
+      rowAverages = 0;
+    }
   }
 
-  public void columnAverages(){
+  public void columnAverages()
+  {
+    //Column major order
+    // take each column and go through all of the rows
+    // top down and left to right
 
+    int colAverages = 0;
+    for(int col = 0; col<ray.length; col++)
+    {
+      for(int row = 0; row<ray[col].length; row++)
+      {
+        colAverages += ray[row][col];
+      }
+      colAverages /= ray[col].length;
+      System.out.println("The Average for col " + col + " is: " + colAverages);
+      colAverages = 0;
+    }
   }
 
-  public void arrayAverage(){
-
+  public void arrayAverage()
+  { 
+    int rayAverage = 0;
+    for(int row = 0; row<ray.length; row++)
+    {
+      for(int col = 0; col<ray[row].length; col++)
+      {
+        rayAverage += ray[row][col];
+      }
+      
+    }
+    rayAverage /= ray.length;
+    System.out.println("The Average for the array is: " + rayAverage);
   }
 
   public String toString(){
@@ -25,5 +63,8 @@ public class ArrayAverages{
     {
       System.out.println(Arrays.toString(ray[row]));
     }
+    
+    return "";
+
   }
 }
--- Gee17.java
+++ Gee17.java
@@ -1,7 +1,30 @@
-public class Gee17{
-  public static void main(String[] args){
-    TwoDee twoDee = new TwoDee();
+public class Gee17
+{
+  public static void main(String[] args)
+  {
+    int[][] ray = new int[7][7];
 
+    int rayNum = 0;
+
+    //Populate the array with sequential numbers
+    /*
+     1  2  3  4   5   6   7
+     8  9  10 11  12  13  14
+     15 16 17 18  19  20  21
+     22 23 24 25...
+    */
+    for(int row = 0; row<ray.length; row++)
+    {
+      for(int col = 0; col<ray[row].length; col++)
+      {
+        ray[row][col] = rayNum;
+        rayNum++;
+      }
+    }
+
+    ArrayAverages twoDee = new ArrayAverages(ray);
+
+    System.out.println(twoDee);
     twoDee.rowAverages();
     twoDee.columnAverages();
     twoDee.arrayAverage();
